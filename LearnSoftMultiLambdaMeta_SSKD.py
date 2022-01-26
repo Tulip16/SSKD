1e6e89e4f93a57a27a1de210c3031af9fcd8f956
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2022-01-26 11:29:52 +0530

    Update LearnSoftMultiLambdaMeta_SSKD.py

--- LearnSoftMultiLambdaMeta_SSKD.py
+++ LearnSoftMultiLambdaMeta_SSKD.py
@@ -77,6 +77,7 @@ class LearnSoftMultiLambdaMeta(object):
         with torch.no_grad():
 
             for batch_idx, (inputs, targets, _) in enumerate(self.valloader):
+                print(inputs.size())
                 inputs, targets = inputs.to(self.device), targets.to(self.device, non_blocking=True)
                 print(inputs.size())
                 if batch_idx == 0:
