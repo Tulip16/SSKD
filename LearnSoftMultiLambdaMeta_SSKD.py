a171b660051799e141e634776b5d24c4835cb2bb
jingax <aastik.solanki99@gamil.com>
2022-01-31 00:10:59 +0530

    q

--- LearnSoftMultiLambdaMeta_SSKD.py
+++ LearnSoftMultiLambdaMeta_SSKD.py
@@ -128,7 +128,7 @@ class LearnSoftMultiLambdaMeta(object):
             inputs = inputs.view(-1,c,h,w).cuda()
 
             outputs, l1, i_, _ = self.model(inputs)
-            custom_target=torch.cat((target,target,dim=0))
+            custom_target=torch.cat((target,target),dim=0)
             custom_target=torch.cat((custom_target,custom_target),dim=0)
             
             # for i in range(targets.size()[0]):
