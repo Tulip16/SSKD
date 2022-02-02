9d76b1388b4715372c5a37636013f9eee879491a
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2022-02-03 00:14:14 +0530

    updated input sizes

--- LearnSoftMultiLambdaMeta_SSKD.py
+++ LearnSoftMultiLambdaMeta_SSKD.py
@@ -124,9 +124,10 @@ class LearnSoftMultiLambdaMeta(object):
             #batch_wise_indices = list(self.trainloader.batch_sampler)
             # print("input for SL grads before tranformation", inputs.size())
             inputs, target = inputs.to(self.device), target.to(self.device, non_blocking=True)
-            inputs = inputs[:,0,:,:,:].cuda()
-            # c,h,w = inputs.size()[-3:]
-            # inputs = inputs.view(-1,c,h,w).cuda()
+            # inputs = inputs[:,0,:,:,:].cuda()
+            c,h,w = inputs.size()[-3:]
+            inputs = inputs.view(-1,c,h,w).cuda()
+            inputs = inputs[0:4:-1,:,:,:]
             # print("input for SL grads after tranformation", inputs.size())
 
             outputs, l1, i_, _ = self.model(inputs)
