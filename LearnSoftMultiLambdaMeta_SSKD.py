4981c6523d85f9964df740e6d48def30ec582810
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2022-02-07 14:41:17 +0530

    Update LearnSoftMultiLambdaMeta_SSKD.py

--- LearnSoftMultiLambdaMeta_SSKD.py
+++ LearnSoftMultiLambdaMeta_SSKD.py
@@ -420,6 +420,35 @@ class LearnSoftMultiLambdaMeta(object):
                     del l0_grads
                     del l1_grads
                     del l0_expand
+
+            del alpha_grads_ss
+            del alpha_grads_t
+            del alpha_grads
+            del grad
+            del grad_SS
+            del grad_T
+            del loss_SL
+            del loss_SS
+            del loss_KD
+            del loss_T
+            del outputs
+            del l0_grads
+            del l1_grads
+            del l0_expand
+            del alpha_grads_ss
+        del alpha_grads_t
+        del alpha_grads
+        del grad
+        del grad_SS
+        del grad_T
+        del loss_SL
+        del loss_SS
+        del loss_KD
+        del loss_T
+        del outputs
+        del l0_grads
+        del l1_grads
+        del l0_expand
                 
 
         #lambdas.clamp_(min=0.01,max=0.99)
